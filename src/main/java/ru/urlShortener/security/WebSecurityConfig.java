20e3ca0f9fddbc33dc41a7ca145b2b04d90ee5b7
ilshat <ilshatrashidovich@rambler.ru>
2019-09-12 18:20:15 +0500

    Deploy to heroku

--- src/main/java/ru/urlShortener/security/WebSecurityConfig.java
+++ src/main/java/ru/urlShortener/security/WebSecurityConfig.java
@@ -35,10 +35,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.requiresChannel().anyRequest().requiresSecure();
-                .requiresChannel()
+                .requiresChannel().anyRequest().requiresSecure();
+                /*.requiresChannel()
                 .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
+                .requiresSecure();*/
 
         http
                 .authorizeRequests()
