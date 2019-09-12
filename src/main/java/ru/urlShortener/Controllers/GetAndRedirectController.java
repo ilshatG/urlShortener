831b362112d48ad5c5bbb94e96c905e16ff57c92
ilshat <ilshatrashidovich@rambler.ru>
2019-09-12 21:23:52 +0500

    Deploy to heroku

--- src/main/java/ru/urlShortener/Controllers/GetAndRedirectController.java
+++ src/main/java/ru/urlShortener/Controllers/GetAndRedirectController.java
@@ -14,6 +14,9 @@ import ru.urlShortener.service.DecimalLetterCoder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Optional;
@@ -26,7 +29,7 @@ import ru.urlShortener.service.LocalTools;
 public class GetAndRedirectController implements ErrorController {
 
 
-    @Value("classpath:static/index.html")
+    @Value(/*classpath:*/ "/static/index.html")
     Resource resourceFile;
 
     @Autowired
